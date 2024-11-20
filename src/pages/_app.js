0f8a21a71d6c1a08acc65152fe59da9778dc301c
Rohit Gupta <Rohitg8812@gmail.com>
2024-11-20 10:19:29 +0530

    remove the extra semicolon from the bottom of webpage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps }) {
           },
         }}
       />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
